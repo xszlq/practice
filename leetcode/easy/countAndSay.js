4b62ced6a351ae9ed1b9c6ade6306d4061acf87f
LiuQiang <xszlq@foxmail.com>
2019-02-19 21:11:45 +0800

    feat: solve count and say

--- leetcode/easy/countAndSay.js
+++ leetcode/easy/countAndSay.js
@@ -8,5 +8,37 @@
  * @return {string}
  */
 var countAndSay = function(n) {
+    function getNext(before){
+        let now,next,rv="";
+        let length = 1;
+        for(let i=0, j=0; i<before.length; i++){
+            j++;
+            now = before.charAt(i);
+            next = before.charAt(j);
 
+            if(next){
+                if(next === now){
+                    length++;
+                }else{
+                    rv += length+now;
+                    length = 1;
+                }
+            }else{
+                rv += length + now;
+            }
+        }
+
+        return rv;
+    }
+
+    let begin = "1", rv = "1";
+    for(let i=1; i<n; i++){
+        rv = getNext(begin);
+        begin = rv;
+    }
+
+    return rv;
 };
+
+
+console.log(countAndSay(7));
